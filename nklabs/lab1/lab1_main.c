e0fe0ca55f5bb5d81f530dfea31fbecee3eb181b
Yvan Pearson <ypearson@nklabs.com>
2015-08-06 13:43:56 -0400

    tested nuttx os api using nklabs test apps.

--- nklabs/lab1/lab1_main.c
+++ nklabs/lab1/lab1_main.c
@@ -3,30 +3,127 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+
+#include <stdbool.h>
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sched.h>
+#include <errno.h>
+#include <semaphore.h>
 
 /****************************************************************************
- * Definitions
+ * Pre-processor Definitions
  ****************************************************************************/
-
+#define STACKSIZE 1024
+#define PRIORITY   100
 /****************************************************************************
  * Private Data
  ****************************************************************************/
+static sem_t sem;
 
+/****************************************************************************
+ * Private Functions
+ ****************************************************************************/
+int lab1_main(int argc, char *argv[])
+{
+  task_test();
+  return 0;
+}
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
+ void task1(void)
+ {
 
-/****************************************************************************
- * NK Labs: Lab1 
- ****************************************************************************/
+  int status, value;
+  sleep(3);
+  status = sem_getvalue(&sem, &value);
 
-#ifdef CONFIG_BUILD_KERNEL
-int main(int argc, FAR char *argv[])
-#else
-int lab1_main(int argc, char *argv[])
-#endif
+  if (status < 0)
+    {
+      printf("task1: ERROR thread %d could not get semaphore value\n");
+    }
+  else
+    {
+      printf("task1: Thread semaphore value = %d\n", value);
+    }
+
+  printf("task1: Thread posting semaphore\n");
+  
+  status = sem_post(&sem);
+  
+  if (status != 0)
+    {
+      printf("task1: ERROR thread sem_wait failed\n");
+    }
+   
+ }
+
+ void task2(void)
+ {
+  int status, value;
+
+  status = sem_getvalue(&sem, &value);
+  
+  if (status < 0)
+    {
+      printf("task2: ERROR thread could not get semaphore value\n");
+    }
+  else
+    {
+      printf("task2: Thread initial semaphore value = %d\n", value);
+    }
+
+  printf("task2: Thread waiting on semaphore\n");
+
+  status = sem_wait(&sem);
+
+  if (status != 0)
+    {
+      printf("task2: ERROR thread sem_wait failed\n");
+    }
+
+  printf("task2: Thread awakened\n");
+
+ }
+ 
+
+void task_test(void)
 {
-  printf("NK: Welcome to Lab1.\n");
-  return 0;
+  int ret;
+  pid_t pid;
+
+  ret = sem_init(&sem, 0, 0);
+  if (ret < 0)
+    {
+      printf("main: sem_init() ERROR.\n");
+    }
+
+  /* Start the task */
+  ret = task_create("task1", PRIORITY, STACKSIZE, task1, 0);
+  if (ret < 0)
+    {
+      printf("main: ERROR Failed to start task1\n");
+    }
+  else
+    {
+      pid = ret;
+      printf("main: Started task1 at PID=%d\n", pid);
+    }
+
+  /* Start the task */
+  ret = task_create("task2", PRIORITY, STACKSIZE, task2, 0);
+  if (ret < 0)
+    {
+      printf("main: ERROR Failed to start task1\n");
+    }
+  else
+    {
+      pid = ret;
+      printf("main: Started task2 at PID=%d\n", pid);
+    }
+
+  printf("main: Exiting\n");
 }
--- nklabs/lab2/lab2_main.c
+++ nklabs/lab2/lab2_main.c
@@ -3,16 +3,29 @@
  ****************************************************************************/
 
 #include <nuttx/config.h>
+
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <ctype.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <mqueue.h>
+#include <sched.h>
+#include <errno.h>
 
 /****************************************************************************
  * Definitions
  ****************************************************************************/
-
+#define STACKSIZE 1024
+#define PRIORITY   100
+#define PAYLOAD "payload"
+#define PAYLOAD_LEN ( strlen(PAYLOAD)+1 )
 /****************************************************************************
  * Private Data
  ****************************************************************************/
-
+static mqd_t sendq;
+static mqd_t recvq;
 /****************************************************************************
  * Public Functions
  ****************************************************************************/
@@ -21,12 +34,119 @@
  * NK Labs: Lab1 
  ****************************************************************************/
 
-#ifdef CONFIG_BUILD_KERNEL
-int main(int argc, FAR char *argv[])
-#else
 int lab2_main(int argc, char *argv[])
-#endif
 {
-  printf("NK: Welcome to Lab2.\n");
+  start_tasks();
   return 0;
+}
+
+void task3(void)
+{
+    struct mq_attr attr;
+    int status = 0;
+
+    printf("sender_thread: Starting\n");
+
+    /* Fill in attributes for message queue */
+
+    attr.mq_maxmsg  = 1;
+    attr.mq_msgsize = PAYLOAD_LEN;
+    attr.mq_flags   = 0;
+
+    sendq = mq_open("mqueue", O_RDWR|O_CREAT, 0666, &attr);
+    if (sendq == (mqd_t)-1)
+    {
+      printf("sender_thread: ERROR mq_open failed\n");
+      pthread_exit((pthread_addr_t)1);
+    }
+
+    sleep(1);
+    status = mq_send(sendq, PAYLOAD, PAYLOAD_LEN, 42);
+    if (status < 0)
+    {
+      printf("sender_thread: ERROR mq_send failure=%d on msg %d\n", status);
+    }
+    else
+    {
+      printf("sender_thread: mq_send succeeded on msg %d\n");
+    }
+
+}
+
+void task4(void)
+{
+    struct mq_attr attr;
+    char msg_buffer[32];
+    int nbytes = 0;
+    int status = 0;
+    int i = 0;
+    memset(msg_buffer, 0x00, PAYLOAD_LEN);
+    printf("sender_thread: Starting\n");
+    //msg_buffer[0] = 'd';
+    attr.mq_maxmsg  = 1;
+    attr.mq_msgsize = PAYLOAD_LEN;
+    attr.mq_flags   = 0;
+
+    recvq = mq_open("mqueue", O_RDWR|O_CREAT, 0666, &attr);
+    if(sendq == (mqd_t)-1)
+    {
+      printf("sender_thread: ERROR mq_open failed\n");
+    }
+
+    nbytes = mq_receive(recvq, msg_buffer, PAYLOAD_LEN, 0);
+    if (nbytes < 0)
+    {
+        if (errno != EINTR)
+        {
+            printf("receiver_thread: ERROR mq_receive failure on msg %d, errno=%d\n", errno);
+        }
+        else
+        {
+            printf("receiver_thread: mq_receive interrupted!\n");
+        }
+    }
+    else if (nbytes != strlen("payload")+1)
+    {
+          printf("receiver_thread: mq_receive return bad size %d on msg %d\n", nbytes);
+    }
+    else
+    {
+        printf("receiver_thread: mq_receive succeeded on msg %d\n");
+        printf("MSG = [");
+        while( *(msg_buffer+i) )
+        {
+          printf("%c", msg_buffer[i++]);
+
+        }
+        printf("]\n");
+    }
+}
+
+void start_tasks(void)
+{
+   pid_t pid;
+   int ret;
+   /* Start the task */
+  ret = task_create("task3", PRIORITY, STACKSIZE, task3, 0);
+  if (ret < 0)
+    {
+      printf("main: ERROR Failed to start task1\n");
+    }
+  else
+    {
+      pid = ret;
+      printf("main: Started task1 at PID=%d\n", pid);
+    }
+
+  /* Start the task */
+  ret = task_create("task4", PRIORITY, STACKSIZE, task4, 0);
+  if (ret < 0)
+    {
+      printf("main: ERROR Failed to start task1\n");
+    }
+  else
+    {
+      pid = ret;
+      printf("main: Started task2 at PID=%d\n", pid);
+    }
 }
